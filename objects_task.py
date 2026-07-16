68c5752c08c60706043d54c796edc532e01f7f12
John <stphn0445@gmail.com>
2026-07-16 12:15:19 +0300

    Using flask and html

--- objects_task.py
+++ objects_task.py
@@ -1,23 +1,31 @@
+# task OOP Task 1.Create a class called BankAccount with the following attributes:
+#  -account number -balance -owner name -date opened 
+# 2.Give the above BankAccount class the following behaviour or methods: -deposit() -withdraw() -display_info()
+#  3.Create two BankAccount objects that can deposit, withdraw and display_info
+from datetime import datetime
+today = datetime.today()
 class BankAccount:
-    def __init__(self,account_number,balance,owner_name,date_opened):
+    def __init__(self,account_number,balance,owner_name,date_opened=today):
         self.account_name = account_number
         self.balance = balance
         self.owner_name = owner_name
         self.date_opened = date_opened
     def withdraw(self):
-        self.amount = int(input('Enter amount to withdraw: '))
-        return self.balance-self.amount
+        amount = int(input('Enter amount to withdraw: '))
+        self.balance = self.balance-amount
+        return self.balance
 
     def deposit(self):
-        self.amount = int(input('Enter amount to deposit: '))
-        return self.balance+self.amount
+        amount = int(input('Enter amount to deposit: '))
+        self.balance = self.balance + amount
+        return self.balance
     def display_info(self):
         return self.balance
 
-account_1 = BankAccount(1234,4000,'Jane',2020-12-12)
-account_2 = BankAccount(5467,5000,'Mary',2021-10-23)
-# print(account_1.withdraw())
-# print(account_1.deposit())
-print(account_1.display_info())
+account_1 = BankAccount(1234,4000,'Jane')
+account_2 = BankAccount(5467,5000,'Mary')
+print(account_2.withdraw())
+print(account_2.deposit())
+print(account_2.display_info())
 
 
