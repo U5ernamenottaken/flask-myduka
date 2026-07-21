4e1af6dc263d415a31889494ec09ecb41f385913
John <stphn0445@gmail.com>
2026-07-21 10:44:46 +0300

    Displaying data using jinja

--- database.py
+++ database.py
@@ -31,7 +31,7 @@ def insert_sales(sales_values):
 #insert_sales(sale1)
 sales = get_sales()
 
-print(sales)
+#print(sales)
 #Viewing data into the stock table 
 def get_stock():
     cur.execute("select*from stock")
@@ -44,7 +44,6 @@ def insert_stock(stock_values):
 #stock1 = (2,300) 
 #insert_stock(stock1)
 stock = get_stock()
-print(stock)
 
 #calculating the sales per product
 def sales_per_product():
@@ -55,7 +54,7 @@ def sales_per_product():
     sales_product = cur.fetchall()
     return sales_product
 sales_prod = sales_per_product()
-print(sales_prod)
+
 
 #sales per day
 def sales_per_day():
@@ -66,7 +65,7 @@ def sales_per_day():
     sales_day = cur.fetchall()
     return sales_day
 sale_day = sales_per_day()
-print(sale_day)
+
 
 #profit per day
 def profit_per_day():
